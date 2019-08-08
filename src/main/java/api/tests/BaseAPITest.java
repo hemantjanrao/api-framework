3aa9d7a237c96912db04abd42af3f0bcdc18fdf6
hjanrao <hjanrao@wayfair.com>
2019-08-08 16:51:39 +0200

    Added test listner

--- src/main/java/api/tests/BaseAPITest.java
+++ src/main/java/api/tests/BaseAPITest.java
@@ -1,8 +1,11 @@
 package api.tests;
 
+import common.listners.TestListner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListner.class)
 public abstract class BaseAPITest {
 
     protected final Logger logger = LogManager.getLogger(this);
